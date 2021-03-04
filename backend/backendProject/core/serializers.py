4809ff4267ccd673bb88485523800912e524aed2
thallyssonklein <thallyssonklein@gmail.com>
2021-03-04 07:01:06 -0300

    Returning nested object at /contact

--- backend/backendProject/core/serializers.py
+++ backend/backendProject/core/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Contact, Address
 
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ['id', 'name', 'phone', 'email', 'createdAt', 'addresses']
-
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ['cep', 'address', 'contact']
+
+class ContactSerializer(serializers.ModelSerializer):
+    addresses = AddressSerializer(many=True)
+
+    class Meta:
+        model = Contact
+        fields = ['id', 'name', 'phone', 'email', 'createdAt', 'addresses']
